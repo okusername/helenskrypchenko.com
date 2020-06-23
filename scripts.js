73e25d30a328299fbdfe97a02bc472c7887ac700
okusername <62268503+okusername@users.noreply.github.com>
2020-06-23 20:01:56 +0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,33 +4,33 @@
 
 // CHANGE SLIDES ON MAIN SCREEN
 
-var count = 1; // start after all images loaded
+
+var count = 1; 
 
 function mySlidesLoaded(){
     ++count;
     console.log(count);
     if (count === 8){
+        carousel();
         return count;
     }
 }
 
-// console.log(count);
-
-var myIndex = 0; // main function
-carousel();
+var myIndex = 0;
 
 function carousel(){
     var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
     }
     myIndex++;
-    if (myIndex > x.length && count === 8) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-
+    if (myIndex > x.length) {myIndex = 1}
+    x[myIndex-1].style.display = "block";
     setTimeout(carousel, 1000);
 }
+
+
 
 
 
